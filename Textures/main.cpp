e89627f7aafd599d2d6cab682b900a4f2d019771
Canyon Davidson <canyon.davidson@gmail.com>
2024-09-11 23:28:55 -0400

    wooden texture applied to a square

--- Textures/main.cpp
+++ Textures/main.cpp
@@ -1,45 +1,76 @@
 // Core Libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <direct.h>
 
 // Personal/External Libraries
 #include "OpenGLOperations.h"
 #include "Shader.h"
 #include "stb_image.h"
 
+// TODO: continue on "Texture Units" section of Textures lesson in OpenGL
+
 int main()
 {
 	GLFWwindow* window = InitializeOpenGL("Textures");
+	Shader woodenShader("WoodenTileVertexShader.glsl", "WoodenTileFragmentShader.glsl");
 
-	Shader brickShader("", "");
-
-	float Triangle1[] = {
-		-1.0f, 0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+	float TileVertices[] = {
+		// positions        // colors           // texture coords
+		0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,  // bottom left
+		-0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f   // top left
 	};
 
-	float brickTextureCords[] = {
-		0.0f, 0.0f, // lower left corner
-		1.0f, 0.0f, // lower right corner
-		0.5f, 1.0f // top center
+	unsigned int ShapeIndices[] =
+	{
+		0, 1, 3,
+		1, 2, 3
 	};
 
+	// initialize VAO & VBO
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(TileVertices), TileVertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ShapeIndices), ShapeIndices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	// Initialize texture
 	unsigned int woodenTexture;
 	glGenTextures(1, &woodenTexture);
+	glBindTexture(GL_TEXTURE_2D, woodenTexture);
 
 	// set texture wrapping/filtering options on currently bound texture object
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE, woodenTexture);
 
 	// load texture image
 	int width, height, nrChannels;
-	unsigned char* woodenTextureData = stbi_load("Textures//TextureResources//woodencontainer.jpg", &width, &height, &nrChannels, 0);
+	unsigned char* woodenTextureData = stbi_load("TextureResources//woodencontainer.jpg", &width, &height, &nrChannels, 0);
 	if (woodenTextureData)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, woodenTextureData);
@@ -49,23 +80,7 @@ int main()
 	{
 		std::cout << "Failed to load texture" << std::endl;
 	}
-
-	
 	stbi_image_free(woodenTextureData);
-
-	// initialize VAO & VBO
-	unsigned int VAO;
-	unsigned int VBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle1), Triangle1, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	
-	glViewport(0, 0, 800, 600);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -75,12 +90,13 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//render shader
-		brickShader.Use();
+		// bind Texture
+		glBindTexture(GL_TEXTURE_2D, woodenTexture);
 
-		//Bind VAO 1
+		//render shader
+		woodenShader.Use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
 		//Swap color buffer and shows output to screen
 		glfwSwapBuffers(window);
@@ -91,7 +107,7 @@ int main()
 
 
 	// Clean-up
-	glDeleteProgram(brickShader.ID);
+	glDeleteProgram(woodenShader.ID);
 	glfwTerminate();
 
 	return 0;
